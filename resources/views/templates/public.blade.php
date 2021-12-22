bb122bb57affabda471539f971d1d36a83ecbb8a
bennybat50 <benny.bat51@gmail.com>
2021-12-22 23:36:56 +0100

    Update What we do page

--- resources/views/templates/public.blade.php
+++ resources/views/templates/public.blade.php
@@ -243,8 +243,7 @@
         </div>
       </div>
       <a href="#Top" data-w-id="721504d5-3017-a70d-1b78-49ff5a36f591" style="opacity:0;display:none" class="go-up-button w-inline-block"></a>
-
-    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=60c279bbb5649726a8df1402" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="{{ asset('assets/js/webflow.js') }}" type="text/javascript"></script>
 
   </body>
