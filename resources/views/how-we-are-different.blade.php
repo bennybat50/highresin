d9304b7297a721eb2c2feb88198496623c8c3743
jodyson24 <joshua.adegbola20@gmail.com>
2021-12-23 18:59:33 +0100

    More changes

--- resources/views/how-we-are-different.blade.php
+++ resources/views/how-we-are-different.blade.php
@@ -100,7 +100,7 @@
 
             </div>
         </div>
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
         <div class="row">
             <div class="col-lg-6">
 
--- resources/views/research.blade.php
+++ resources/views/research.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.public')
 @section('content')
 
-<div class="what-we-do-welcome">
+<div class="what-we-do-welcome research_welcome">
     <div class="welcome-sec text-center">
         <div class="container">
             <div class="p-5">
@@ -11,11 +11,237 @@
                 </h1>
                 <br>
                 <p class="text-white font-20 pr-5">
-                    
+
                 </p>
                 <br>
                 <a href="" class="site-btn-white">Get Started</a>
             </div>
+        </div>
+    </div>
+</div>
+
+<div class="content_section py-5">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Understanding Whales & Market Movers</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>2 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            In the cryptocurrency market, “Whales” can manipulate the price of an asset by selling off
+                            large
+                            quantities to the market in a very short period of time. Whales can be individuals or a
+                            group of
+                            people/funds that typically hold at least 1,000 BTC.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Common Trading Mistakes</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>2 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            Thomas J. Watson used to say “If you want to increase your success rate, double your failure
+                            rate”. Crypto traders know that Watson’s words are nothing but the truth. With the recent
+                            crypto-trading boom, the idea of trading being “easy” has become increasingly popular.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Diversification</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>2 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            Having a diversified portfolio is not only for stock market traders, every investor should
+                            manage portfolio theories! A proper understanding of coins categorization will help you to
+                            create a diversified portfolio. Balancing the risk is an important part of the game and
+                            investors must know how to limit their losses in such a volatile market.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Smart Contracts</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>2 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            A Smart Contract is a type of computer program that usually underpins the transfer of
+                            digital assets from one party to another. The “terms” of the agreement between the two
+                            parties are stored on the blockchain, and therefore can be easily tracked but once set,
+                            cannot be altered.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Swing Trading vs Day Trading</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>3 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            When it comes to Cryptocurrencies, there are 3 different approaches, essentially decided by
+                            timeframes – long, medium, and short term. Long-term investments are generally left alone,
+                            or added to overtime. However medium (swing trading) and short-term (day trading) can be
+                            very different.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Circulating vs Total Supply</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>2 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            Market demand and supply are two of the most important factors determining a coin’s value,
+                            thus, every trader should have a clear understanding of the effects of circulating supply
+                            and total supply. Before investing or mining a cryptocurrency, every investor must do his
+                            own research, in order to build the proper profile of the project he is putting trust in.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">What is “Market Cap”?</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>4 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            Also called market capitalisation, the term refers to the value of a cryptocurrency (mostly
+                            measured in US Dollars). The metric is widely used in both, fundamental and technical
+                            analysis and it gives traders a clear sign of the financial value acquired in a certain
+                            asset.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Mindset for Crypto</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>2 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            The cryptocurrency market is still in its infancy and one must keep this in mind when
+                            approaching it. Investing in a young market has its advantages, the most desired one being
+                            high ROIs. Nonetheless, as uncertainty and volatility remain key aspects of this market, one
+                            must not over-expose themself or over-invest. Any unfavorable regulatory decision can pose a
+                            significant setback and take time before further progress towards adoption.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+                <div class="research_box">
+
+                    <div class="p-5">
+                        <h6 class="fs-4">Trader vs Analyst</h6>
+
+                        <div class="d-flex mb-3" style="list-style: none; font-size: 11px;">
+                            <li class="me-3">By HighResin</li>
+                            <li class="me-3">/</li>
+                            <li>3 min read</li>
+                        </div>
+
+                        <p class="" style="font-size: 14px;">
+                            There are many seemingly good analysts that do are not profitable in the market. The reason
+                            is that analyst and trader/portfolio manager are two different job roles. The reason being
+                            that they require two separate sets of skills. Being a good technical analyst boils down to
+                            experience and knowledge. Whereas being a good trader is related to strong psychology and
+                            having a trader’s mindset.
+                        </p>
+
+                        <a href="" class="research_box_link" style="font-size: 12px;">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </div>
