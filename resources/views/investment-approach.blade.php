adb58b688dc020daf91c499a0d897484575cd3ad
bennybat50 <benny.bat51@gmail.com>
2021-12-23 08:05:50 +0100

    Done with Investment approach

--- resources/views/investment-approach.blade.php
+++ resources/views/investment-approach.blade.php
@@ -3,8 +3,181 @@
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-<h1>Investment Approach page</h1>
+<div class="investment-approach-welcome">
+    <div class="welcome-sec text-center">
+        <div class="container">
+            <div class="p-5">
+                <h4 class="text-white"><b><i>INVESTMENT APPROACH</i></b> -</h4>
+                <h1 class="text-white">
+                    We believe in strategic investing
+                </h1>
+                <br>
+                <p class="text-white font-20 pr-5">It has guided how we do business for more than 6 years, and it’s driven by independent thinking and rigorous research. <br> So our clients can be confident that we’ll strive to select the right investments as we help them achieve their objectives.
+                </p>
+                <br>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="investment-approach-section">
+    <div class="section1">
+        <div class="content-section">
+            <div class="container">
+                <div class="p-5">
+                    <h4>Strategic investing means that we don’t stop at surface-level analysis. Instead, we go beyond the numbers. More than 20 of our investment professionals travel the world, visiting the companies they evaluate. It’s this passion for exploration and understanding that has helped inform better decision-making and prudent risk management for our clients.
+                        We want to improve the financial well-being of our clients—beyond just recommending an investment strategy. We stay in touch as your needs evolve and market conditions shift. We help you meet your longer-term goals with an investment strategy that’s specifically designed for you. And we’re transparent about the investment decisions we make for you because we believe an informed investor is a better investor—and a happier client.
+                        Strategic Investing takes us beyond the numbers.</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section2">
+        <div class="content-section">
+            <div class="container">
+               <div class="text-center">
+                <h1><i><b>OUR APPROACH</b></i></h1>
+               </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="img-section">
+                            <div class="top-banner-left"></div>
+                            <img src="{{ asset('assets/images/approach/flexible.jpg') }}" alt="" class="img-left">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                       <div class="p-5">
+                        <h1><i><b>FLEXIBLE</b></i></h1>
+                        <p>Every investor has unique needs. Some want growth or cash flow, others want to avoid certain industries or companies. At Highresin, investment management starts with you. We take the time to understand your long-term goals and specific needs. Only then can we create a tailored portfolio recommendation that focuses on your goals and adapts as markets change.
+                        </p>
+                       </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                       <div class="p-5">
+                        <h1><i><b>DYNAMIC</b></i></h1>
+                        <p>Many investment advisers stick to one style of investing, such as growth, momentum or value. But what happens when their style falls out of favor? That’s when narrow specialization could hurt your portfolio returns. Highresin prefers a flexible approach that can adapt to our forward-looking market views.
+                        </p>
+                       </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="img-section">
+                            <div class="top-banner-right"></div>
+                            <img src="{{ asset('assets/images/approach/dynamic.jpg') }}" alt="" class="img-right">
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="img-section">
+                            <div class="top-banner-left"></div>
+                            <img src="{{ asset('assets/images/approach/discipline.jpg') }}" alt="" class="img-left">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                       <div class="p-5">
+                        <h1><i><b>DISCIPLINED</b></i></h1>
+                        <p>You deserve a manager who does more than just pick assets. Highresin meticulously analyzes markets, identifies the most attractive investment categories and then chooses individual assets, stocks or other securities for your portfolio. Our thorough approach yields result over time.
+                        </p>
+                       </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                       <div class="p-5">
+                        <h1><i><b>COMPREHENSIVE</b></i></h1>
+                        <p>Many investors have an incomplete view of investing—focusing chiefly on a certain market. However, it is not up to half of the value of the world stock market. That’s important because some of the largest, fastest-growing, best-run companies are worldwide. With a comprehensive, global focus, you can increase diversification and take advantage of opportunities many investment managers miss.
+                        </p>
+                       </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="img-section">
+                            <div class="top-banner-right"></div>
+                            <img src="{{ asset('assets/images/approach/comprehensive.jpg') }}" alt="" class="img-right">
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="img-section">
+                            <div class="top-banner-left"></div>
+                            <img src="{{ asset('assets/images/approach/research.jpg') }}" alt="" class="img-left">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                       <div class="p-5">
+                        <h1><i><b>RIGOROUS RESEARCH</b></i></h1>
+                        <p>Over 10 of our investment professionals go out into the field to talk to customers, suppliers, employees, and managers to learn firsthand where a company stands and where it could go in the future.
+                        </p>
+                       </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                       <div class="p-5">
+                        <h1><i><b>DEEP EXPERIENCE </b></i></h1>
+                        <p>Our portfolio managers average years in the industry and years with highresin and they’ve weathered all kinds of markets.                        </p>
+                       </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="img-section">
+                            <div class="top-banner-right"></div>
+                            <img src="{{ asset('assets/images/approach/experience.jpg') }}" alt="" class="img-right">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="section3">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ asset('assets/images/approach/management.jpg') }}" alt="">
+            </div>
+            <div class="col-md-6">
+               <div class="p-5">
+                <h1><b>Prudent Risk Management</b> </h1>
+                <p>We carefully manage risk-seeking to both minimize losses and maximize returns for our clients. After all, gaining more is often a matter of losing less.
+                </p>
+                <h4><i><b>Prudent risk management is essential in all market conditions.</b></i></h4>
+                <p>Helping to limit investors’ losses is just as important as if not more important than delivering growth. In addition to identifying promising investment opportunities, our strategic investing approach helps us prudently manage risk by:
+                </p>
+                <ul>
+                    <li>Combining different types of risk in carefully constructed portfolios to avoid excessive concentration in any one sector, region, or company.</li>
+                    <li>Actively adjusting to the macroeconomic factors that impact volatility, such as global trade events, along with interest rate, credit, and currency risk in global capital markets.</li>
+                    <li>Monitoring risk throughout the investment process through statistical tools and the oversight of senior investment professionals and corporate officers.</li>
+                </ul>
+               </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
