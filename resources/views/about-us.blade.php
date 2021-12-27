cbd1e36889da1547b0dc18d1d4bee412db953aad
bennybat50 <benny.bat51@gmail.com>
2021-12-27 22:50:22 +0100

    Update about page

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -29,7 +29,7 @@
            <div class="p-5">
             <div class="row">
                 <div class="col-md-4 text-center">
-                    <h1>2006</h1>
+                    <h1>2011</h1>
                     <p>Firm Founded</p>
                     <div class="small-line"></div>
                 </div>
@@ -55,7 +55,7 @@
                         <div class="p-5 content">
                             <h1 class="text-white"><b><i>Our</i></b> History</h1>
                             <p class="text-white">Highresin story is about new ideas, innovative thinking and growth through collaboration.
-                                From our humble beginnings in 2015 our founders set out to create better asset management firm “with a reputation for the highest character and the soundest investment philosophy.”
+                                From our humble beginnings in 2011 our founders set out to create better asset management firm “with a reputation for the highest character and the soundest investment philosophy.”
                                 While managing discretionary assets with a fundamental belief in capitalism and free capital markets. We start with the simple notion that supply and demand are the sole determinants of securities pricing. In addition, we believe capital markets are relatively efficient discounters of all widely known information.
                                 <br><br> Thus, to add value through active management, one must identify information not widely known or interpret widely known information differently—and correctly—from other market participants. Throughout Highresin history, we have continuously developed ways to look at the market differently.
 
@@ -210,6 +210,152 @@
         </div>
         <div id="triangle-topright" class="triangle2">
 
+        </div>
+    </div>
+</div>
+<div class="about-what-we-do-sections">
+    <div class="section1 mb-5">
+        <div class="content-section">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-4">
+                        <img src="{{ asset('assets/images/what-we-do/purpose.jpg') }}" alt="">
+                    </div>
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                        <div class="p-5">
+                            <h1><b>A LEGACY OF PURPOSE.</b></h1>
+                            <h4><i><b>We work every day to do right by our clients, so they <br> can invest confidently toward their financial futures.</b></i></h4>
+                            <p>HighResin Group is a global asset management organization with over $15 million in assets under management as of February 28, 2021. The organization provides a broad array of asset classes, mutual funds, sub-advisory services, and separate account management for individual and institutional investors, retirement plans, and financial intermediaries. </p>
+                            <p>HighResin also offers sophisticated investment planning and guidance tools. A disciplined, risk-aware investment approach focuses on diversification, style consistency, and fundamental research.
+
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section2">
+        <div class="content-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5">
+                        <img src="{{asset('assets/images/what-we-do/capabilities.jpg') }}" alt="">
+                        <br>
+                        <br>
+                        <h2>INVESTMENT <b><i>CAPABILITIES</i></b></h2>
+                        <p><b><i>Expertise across all asset classes and the globe.</i></b></p>
+                        <p>As global investment manager, we actively listen, anticipate, and develop strategies that respond to the needs of our clients. </p>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+                        <img src="{{asset('assets/images/what-we-do/approach.jpg') }}" alt="">
+                        <br>
+                        <br>
+                        <h2>INVESTMENT <b><i>APPROACH</i></b></h2>
+                        <p><b><i>Strategic investing means that we don’t stop at surface-level analysis.</i></b></p>
+                        <p>We believe in strategic investing. It has guided how we do business for more than 6 years, and it’s driven by independent thinking and rigorous research. So our clients can be confident that we’ll strive to select the right investments as we help them achieve their objectives.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section3">
+        <div class="section-full">
+            <div data-w-id="12718a2b-c43d-1ff3-8b7c-67e44d0da587" class="absolute section-bg"></div>
+            <div data-w-id="eaf38632-a1ae-f587-c079-0708a1353aa3" style="display:block" class="absolute-bg"></div>
+            <div class="flex-left">
+                <div class="text-container-4">
+                    <div class="_76-percent-column">
+                        <div class="left-padding">
+                            <h2 data-w-id="12718a2b-c43d-1ff3-8b7c-67e44d0da58d"  class="no-top-margin"> <b><i>OUR CULTURE</i></b>
+                            </h2>
+                        </div>
+                        <div class="top-margin _30-pixels">
+                            <p data-w-id="12718a2b-c43d-1ff3-8b7c-67e44d0da590" >The culture of Highresin is built and sustained by our values.
+                                Different perspectives, opinions, and experiences are encouraged to yield the best outcomes for our clients and the firm.
+                                We also celebrate the unique experiences of our associates and foster their commitment to the communities where they work and live.
+                                We believe in the long-term success of our clients, our associates, and the firm.When you work with dedicated professionals who care about your well-being,
+                                 it can give you confidence and peace of mind.Our culture supports our commitment to provide unparalleled service that puts clients’ interests first.
+                            </p>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section4">
+        <div class="content-section">
+            <div class="container">
+                <div class="text-center">
+                    <h1>How we <b><i>Do Things</i></b></h1>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="border-column ">
+                            <h3 class="p-4"><b>Putting Clients First</b></h3>
+                            <div class="vertical-line"></div>
+                            <div class="p-4">
+                                <p>We do things differently than other investment firms—not just to be different, but because it makes a difference for our clients. We designed our entire business to minimize conflicts of interest. We have a strong division of labour between our sales and service roles, allowing employees in each role to focus on their strengths.
+                                    At Highresin, our client service is wholly focused on providing superior service, with no additional sales. This clear focus on servicing our clients is what makes Highresin a better place to work, too.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <div class="border-column ">
+                            <h3 class="p-4"><b>Diversity and Inclusion</b></h3>
+                            <div class="vertical-line"></div>
+                            <div class="p-4">
+                                <p>We do things differently than other investment firms—not just to be different, but because it makes a difference for our clients. We designed our entire business to minimize conflicts of interest. We have a strong division of labour between our sales and service roles, allowing employees in each role to focus on their strengths.
+                                    At Highresin, our client service is wholly focused on providing superior service, with no additional sales. This clear focus on servicing our clients is what makes Highresin a better place to work, too.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <div class="border-column ">
+                            <h3 class="p-4"><b>Opportunity</b></h3>
+                            <div class="vertical-line"></div>
+                            <div class="p-4">
+                                <p>Our employees drive our firm’s success, and we support their development by providing opportunities for everyone to succeed. We hire and promote employees based on their merit, potential and “will-do” attitude.
+                                    Each employee is empowered to achieve excellence as they grow a lifelong Highresin career. While everyone forges their own unique path at Highresin, we are all on a journey together—a journey to better the investment universe.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <div class="border-column ">
+                            <h3 class="p-4"><b>Recognition</b></h3>
+                            <div class="vertical-line"></div>
+                            <div class="p-4">
+                                <p>Hard work and dedication are recognized and rewarded at Highresin because our employees’ contributions are what make our success possible.
+                                    We celebrate wins of all sizes and make sure each employee’s great work does not go unnoticed. Authentic recognition is the norm across every level, role, department and country to encourage and reinforce our values.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 ">
+                        <div class="border-column ">
+                            <h3 class="p-4"><b>Support</b></h3>
+                            <div class="vertical-line"></div>
+                            <div class="p-4">
+                                <p>We support each other. In our open office environment, everyone from entry-level employees to VPs shares the same space and is treated as an equal. Teams are motivated to collaborate on projects and interact without physical or organizational barriers.
+                                    Our customized development programs and award-winning training help employees excel in their current roles while gaining new skills to prepare for their next one. To support employees’ health, wellness and family life, we offer a robust benefits package and on-site services that help make employees’ busy lives a little easier. At Highresin we have each other's backs.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+               </div>
         </div>
     </div>
 </div>
