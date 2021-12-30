74f8a24389ccc754e23844d22827adbe58e37a6e
jodyson24 <joshua.adegbola20@gmail.com>
2021-12-30 18:55:19 +0100

    Investment Capabilitties page skeleton laid out - awaiting images

--- resources/views/our-culture.blade.php
+++ resources/views/our-culture.blade.php
@@ -1,10 +1,45 @@
 @extends('templates.public')
 @section('content')
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<h1>Our Culture page</h1>
+
+<div class="what-we-do-welcome how_we_are_diff_welcome">
+    <div class="welcome-sec text-center">
+        <div class="container">
+            <div class="p-5">
+                <h4 class="text-white"><b><i>Investment Capabilities</i></b> -</h4>
+                <h1 class="text-white">
+
+                </h1>
+                <br>
+                <p class="text-white font-20 pr-5">
+                    As global investment manager, we actively listen, anticipate, and develop strategies that respond to
+                    the needs of our clients.
+                </p>
+                <br>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content_section">
+    <div class="container">
+        <div class="row">
+            <p>
+                Expertise across all asset classes and the globe. As global investment manager, we actively listen,
+                anticipate, and develop strategies that respond to the needs of our clients. Each strategy is supported
+                by our strategic investing approach—an approach driven by independent thinking and guided by the rigor
+                of our global research. Our investment professionals work together across regions, sectors, and asset
+                classes, as they strive to select the right investment opportunities for our clients.
+                <br>
+                <br>
+                With a broad range of equity, fixed income, and asset allocation capabilities, we provide an array of
+                commingled funds, sub-advisory services, separate account management, and related services for advisors,
+                institutions, financial intermediaries, and retirement plan sponsors. This diversity of product and
+                distribution is a source of strength and stability; it reduces our reliance on any one strategy or
+                segment and the potential impact of negative market cycles. As a result, we are able to continuously
+                invest in new capabilities—for the benefit of our clients.
+            </p>
+        </div>
+    </div>
+</div>
+
 @endsection
