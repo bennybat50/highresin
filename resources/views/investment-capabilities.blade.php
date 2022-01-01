3c0d64d8f36f073dd2311986c0d8d5729bfcee39
bennybat50 <benny.bat51@gmail.com>
2022-01-01 10:53:16 +0100

    Update about page

--- resources/views/investment-capabilities.blade.php
+++ resources/views/investment-capabilities.blade.php
@@ -22,36 +22,36 @@
 </div>
 <div class="capabilities-section">
 
-   <div class="section1">
-    <div class="content_section">
-        <div class="container">
-            <div class="row  pt-5">
-                <p class="text-justify p-5" >
-                    Expertise across all asset classes and the globe. As global investment manager, we actively listen,
-                    anticipate, and develop strategies that respond to the needs of our clients. Each strategy is supported
-                    by our strategic investing approach—an approach driven by independent thinking and guided by the rigor
-                    of our global research. Our investment professionals work together across regions, sectors, and asset
-                    classes, as they strive to select the right investment opportunities for our clients.
-                    <br>
-                    <br>
-                    With a broad range of equity, fixed income, and asset allocation capabilities, we provide an array of
-                    commingled funds, sub-advisory services, separate account management, and related services for advisors,
-                    institutions, financial intermediaries, and retirement plan sponsors. This diversity of product and
-                    distribution is a source of strength and stability; it reduces our reliance on any one strategy or
-                    segment and the potential impact of negative market cycles. As a result, we are able to continuously
-                    invest in new capabilities—for the benefit of our clients.
-                </p>
+    <div class="section1">
+        <div class="content_section">
+            <div class="container">
+                <div class="row  pt-5">
+                    <p class="text-justify p-5">
+                        Expertise across all asset classes and the globe. As global investment manager, we actively listen,
+                        anticipate, and develop strategies that respond to the needs of our clients. Each strategy is supported
+                        by our strategic investing approach—an approach driven by independent thinking and guided by the rigor
+                        of our global research. Our investment professionals work together across regions, sectors, and asset
+                        classes, as they strive to select the right investment opportunities for our clients.
+                        <br>
+                        <br>
+                        With a broad range of equity, fixed income, and asset allocation capabilities, we provide an array of
+                        commingled funds, sub-advisory services, separate account management, and related services for advisors,
+                        institutions, financial intermediaries, and retirement plan sponsors. This diversity of product and
+                        distribution is a source of strength and stability; it reduces our reliance on any one strategy or
+                        segment and the potential impact of negative market cycles. As a result, we are able to continuously
+                        invest in new capabilities—for the benefit of our clients.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-   </div>
 
     <div class="section2">
         <div class="tri-content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-6 ">
-                           <div class="p-5">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="pl-5 p-5">
                             <h1 class="text-capitalize fs-1 light-border-bottom mt-5 pb-3 mb-4 text-white">Asset <b><i>Allocation</i></b></h1>
                             <h6 class="fs-3 mb-4 text-white"> <i>Innovation driven by our equity and fixed income</i> </h6>
 
@@ -65,12 +65,12 @@
                                     <li>Portfolios built on a strong foundation of underlying equity and fixed income strategies.</li>
                                 </ul>
                             </div>
-                           </div>
-                        </div>
-                        <div class="col-md-6">
-                            <img src="{{ asset('assets/images/overview/invest_cap/2.jpg') }}" class="insights-img-right" style="border-radius: 10px; height: 300px;" />
                         </div>
                     </div>
+                    <div class="col-md-6">
+                        <img src="{{ asset('assets/images/overview/invest_cap/2.jpg') }}" class="insights-img-right" style="border-radius: 10px; height: 300px;" />
+                    </div>
+                </div>
             </div>
         </div>
         <div id="triangle-topleft" class="triangle">
@@ -78,101 +78,163 @@
         </div>
     </div>
 
-    <div class="content-section py-5">
-        <h1 class="text-center ">Things that set us apart</h1>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 p-4">
-                    <div class="card blue">
-                        <h3>OUR PEOPLE ARE OUR GREATEST ASSETS</h3>
-                        <p>We thrive because our company culture is based on collaboration and diversity of thought.</p>
+    <div class="section3">
+        <div class="content-section py-5">
+            <h1 class="text-center ">Things that <br> set us apart</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 p-4">
+                        <div class="card blue">
+                            <h3>OUR PEOPLE ARE OUR GREATEST ASSETS</h3>
+                            <p>We thrive because our company culture is based on collaboration and diversity of thought.</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-md-6 p-4">
-                    <div class="card grey">
-                        <h3>WE INVEST IN OUR PEOPLE.</h3>
-                        <p>We strive for equality in our workforce and continuously working to maintain an environment and
-                            culture our associates are proud to be a part of.</p>
+                    <div class="col-md-6 p-4">
+                        <div class="card grey">
+                            <h3>WE INVEST IN OUR PEOPLE.</h3>
+                            <p>We strive for equality in our workforce and continuously working to maintain an environment and
+                                culture our associates are proud to be a part of.</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-md-6 p-4">
-                    <div class="card grey">
-                        <h3>MAKING AN IMPACT BEYOND OUR WALLS.</h3>
-                        <p>Our associates do more than come in to work each day. They are volunteers, donors, educators,
-                            and environmentalists committed to creating positive change in our communities.</p>
+                    <div class="col-md-6 p-4">
+                        <div class="card grey">
+                            <h3>MAKING AN IMPACT BEYOND OUR WALLS.</h3>
+                            <p>Our associates do more than come in to work each day. They are volunteers, donors, educators,
+                                and environmentalists committed to creating positive change in our communities.</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-md-6 p-4">
-                    <div class="card blue">
-                        <h3>A WORKPLACE FOR ALL.</h3>
-                        <p>A collaborative culture that supports diversity means we all benefit from different backgrounds,
-                            perspectives, and experiences.</p>
+                    <div class="col-md-6 p-4">
+                        <div class="card blue">
+                            <h3>A WORKPLACE FOR ALL.</h3>
+                            <p>A collaborative culture that supports diversity means we all benefit from different backgrounds,
+                                perspectives, and experiences.</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="content_section">
+    <div class="section4">
         <div class="container">
-            <h3 class="fs-1 text-capitalize fw-bold text-center mb-4">More than Investing</h3>
             <div class="row">
-                <div class="col-md-4 p-4">
-                    <h4 class="text-captalize fs-3 pb-3 mb-4 light-border-bottom">A tailored Portofolio</h4>
-                    <p class="fs-5">
-                        Your portfolio recommendation is based on an in-depth understanding of your specific needs.
-                    </p>
-                </div>
-
-                <div class="col-md-4 p-4">
-                    <h4 class="text-captalize fs-3 pb-3 mb-4 light-border-bottom">A DEDICATED INVESTMENT COUNSELOR</h4>
-                    <p class="fs-5">
-                        You will have a dedicated Investment Counselor who provides personal attention and ongoing service.
-                    </p>
-                </div>
-
-                <div class="col-md-4 p-4">
-                    <h4 class="text-captalize fs-3 pb-3 mb-4 light-border-bottom">AN EXPERIENCED MANAGEMENT TEAM</h4>
-                    <p class="fs-5">
-                        Our management team leads by example in putting clients first—in portfolio management and client
-                        service.
-                    </p>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6 p-5">
-                    <h4 class="text-captalize fs-3 pb-3 mb-4 light-border-bottom">
-                        FAIR FEES THAT ARE EASY-TO-UNDERSTAND
-                    </h4>
-                    <p class="fs-5">
-                        Our fee structure is simple: You pay a competitive flat fee that’s based on your portfolio’s size.
-                        This fee structure aligns our interests with yours. In other words, if you do better, we do better.
-                        Our investment approach isn’t swayed by incentives to sell you specific products because, unlike
-                        many brokers, we don’t earn commissions on trades. That means we have no incentive to “churn” your
-                        account with lots of buys and sells.
-                    </p>
+                <div class="col-md-6">
+                    <div class="p-5">
+                        <img src="{{ asset('assets/images/capabilities/dwn-2.png') }}" alt="">
+                    </div>
                 </div>
                 <div class="col-md-6">
-                    <img src="{{ asset('assets/images/overview/invest_cap/3.jpg') }}" class="insights-img-right" style="border-radius: 10px; height: 300px;" />
+                    <div class="p-5">
+                        <h1 class="light-border-bottom">A <b><i>Welcoming <br> Culture</i></b> for All</h1>
+                        <br>
+                        <p>Diversity requires more than good intentions. It’s about putting thought into action. At Highresin, we know that a diversity of experiences lead to greater teamwork and inventive solutions to modern-day problems. At Highresin, we let you be you.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-6">
-                <img src="{{ asset('assets/images/overview/invest_cap/4.jpg') }}" class="insights-img-right" style="border-radius: 10px; height: 300px;" />
+    <div class="section5">
+        <div class="tri-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="pl-5 p-5">
+                            <h1 class="text-capitalize fs-1 light-border-bottom mt-5 pb-3 mb-4 text-dark">Advance
+                                Professionally. <br> <b><i>Succeed
+                                    Personally.</i></b></h1>
+
+                            <div class="fs-4 text-wrap">
+                                <p>We provide clearly defined, transparent development pathways to help you grow both personally and professionally. Because helping you reach your goals is our goal.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="{{ asset('assets/images/capabilities/getty-01.jpeg') }}" class="insights-img-right" style="border-radius: 10px; height: 300px;" />
+                            </div>
+                            <div class="col-md-6">
+                                <img src="{{ asset('assets/images/capabilities/getty-02.jpeg') }}" class="insights-img-right" style="border-radius: 10px; height: 300px;" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-6 p-5">
-                <h4 class="text-captalize fs-3 pb-3 mb-4 light-border-bottom">
-                    TRANSPARENT FEES</h4>
-                <p class="fs-5">
-                    You pay us a fee based on your portfolio size.
-                </p>
+        </div>
+        <div id="triangle-topleft" class="triangle">
+
+        </div>
+    </div>
+
+    <div class="section6">
+        <div class="content-section">
+            <div class="container">
+                <h3 class="fs-1 text-capitalize fw-bold text-center mb-4">More than Investing</h3>
+                <br>
+                <div class="row">
+                    <div class="col-md-4 p-4">
+                        <h1 class="text-captalize fs-3 pb-3 mb-4 light-border-bottom"><i><b>Discover</b></i> <br>
+                            Your Path</h1>
+                        <p class="fs-5">
+                            Through data, insights, and advanced technology, we shine a light on the future. And we need people with multifaceted talents to do it, across disciplines, businesses and teams. Come see what we’re all about.
+
+                        </p>
+                    </div>
+
+                   <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-6 ">
+                            <h4 class="text-captalize fw-bold ">A tailored Portofolio</h4>
+                            <p class="fs-5">
+                                You will have a dedicated Investment Counselor who provides personal attention and ongoing service.
+                            </p>
+                        </div>
+
+                        <div class="col-md-6 ">
+                            <h4 class="text-captalize fw-bold  ">An Experienced Management Team</h4>
+                            <p class="fs-5">
+                                Our management team leads by example in putting clients first—in portfolio management and client
+                                service.
+                            </p>
+                        </div>
+                       </div>
+                       <div class="row">
+                        <div class="col-md-6 ">
+                            <h4 class="text-captalize fw-bold  ">A Dedicated Investment Counselor</h4>
+                            <p class="fs-5">
+                                You will have a dedicated Investment Counselor who provides personal attention and ongoing service.
+                            </p>
+                        </div>
+
+                        <div class="col-md-6">
+                            <h4 class="text-captalize fw-bold ">Transparent Fee</h4>
+                            <p class="fs-5">
+                                You pay us a fee based on your portfolio size.
+                            </p>
+                        </div>
+                        <div class="col-md-12">
+                            <h4 class="text-captalize fw-bold ">Fair Fees that are Easy to understand</h4>
+                            <p class="fs-5">
+                                Our fee structure is simple: You pay a competitive flat fee that’s based on your portfolio’s size.
+                                This fee structure aligns our interests with yours. In other words, if you do better, we do better.
+                                Our investment approach isn’t swayed by incentives to sell you specific products because, unlike
+                                many brokers, we don’t earn commissions on trades. That means we have no incentive to “churn” your
+                                account with lots of buys and sells.
+                            </p>
+                        </div>
+                       </div>
+
+                   </div>
+                </div>
+
+
             </div>
+
         </div>
     </div>
 </div>
