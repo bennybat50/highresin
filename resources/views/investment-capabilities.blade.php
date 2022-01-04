5845239a51aa21113b8d1abd02bc01e33a23b254
bennybat50 <benny.bat51@gmail.com>
2022-01-04 19:23:12 +0100

    Update Pages

--- resources/views/investment-capabilities.blade.php
+++ resources/views/investment-capabilities.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="p-5">
                 <h4 class="text-white"><b><i>Our investment Capabilities</i></b> -</h4>
-               
+
                 <br>
                 <p class="text-white font-20 pr-5 text-wrap text-center">
                     As global investment manager, we actively listen, anticipate, <br> and develop strategies that
@@ -144,7 +144,7 @@
                         <div class="pl-5 p-5">
                             <h1 class="text-capitalize fs-1 light-border-bottom mt-5 pb-3 mb-4 text-dark">Advance
                                 Professionally. <br> <b><i>Succeed
-                                    Personally.</i></b></h1>
+                                        Personally.</i></b></h1>
 
                             <div class="fs-4 text-wrap">
                                 <p>We provide clearly defined, transparent development pathways to help you grow both personally and professionally. Because helping you reach your goals is our goal.</p>
@@ -172,63 +172,70 @@
     <div class="section6">
         <div class="content-section">
             <div class="container">
-                <h3 class="fs-1 text-capitalize fw-bold text-right mb-4">More than Investing</h3>
-                <br>
+                <div class="row ">
+                    <div class="col"></div>
+                    <div class="col-auto">
+                        <h3 class="fs-1 text-capitalize fw-bold  mb-4">More than Investing</h3>
+
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-md-4 p-4">
                         <h1 class="text-captalize fs-3 pb-3 mb-4 light-border-bottom"><i><b>Discover</b></i> <br>
                             Your Path</h1>
                         <p class="fs-5">
                             Through data, insights, and advanced technology, we shine a light on the future. And we need people with multifaceted talents to do it, across disciplines, businesses and teams. Come see what we’re all about.
-
                         </p>
                     </div>
 
-                   <div class="col-md-8">
-                    <div class="row">
-                        <div class="col-md-6 ">
-                            <h4 class="text-captalize fw-bold ">A tailored Portofolio</h4>
-                            <p class="fs-5">
-                                You will have a dedicated Investment Counselor who provides personal attention and ongoing service.
-                            </p>
-                        </div>
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div class="col-md-6 ">
+                                <h4 class="text-captalize fw-bold ">A tailored Portofolio</h4>
+                                <p class="fs-5">
+                                    You will have a dedicated Investment Counselor who provides personal attention and ongoing service.
+                                </p>
+                            </div>
 
-                        <div class="col-md-6 ">
-                            <h4 class="text-captalize fw-bold  ">An Experienced Management Team</h4>
-                            <p class="fs-5">
-                                Our management team leads by example in putting clients first—in portfolio management and client
-                                service.
-                            </p>
+                            <div class="col-md-6 ">
+                                <h4 class="text-captalize fw-bold  ">An Experienced Management Team</h4>
+                                <p class="fs-5">
+                                    Our management team leads by example in putting clients first—in portfolio management and client
+                                    service.
+                                </p>
+                            </div>
                         </div>
-                       </div>
-                    <br>
-                       <div class="row">
-                        <div class="col-md-6 ">
-                            <h4 class="text-captalize fw-bold  ">A Dedicated Investment Counselor</h4>
-                            <p class="fs-5">
-                                You will have a dedicated Investment Counselor who provides personal attention and ongoing service.
-                            </p>
-                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-6 ">
+                                <h4 class="text-captalize fw-bold  ">A Dedicated Investment Counselor</h4>
+                                <p class="fs-5">
+                                    You will have a dedicated Investment Counselor who provides personal attention and ongoing service.
+                                </p>
+                            </div>
 
-                        <div class="col-md-6">
-                            <h4 class="text-captalize fw-bold ">Transparent Fee</h4>
-                            <p class="fs-5">
-                                You pay us a fee based on your portfolio size.
-                            </p>
+                            <div class="col-md-6">
+                                <h4 class="text-captalize fw-bold ">Transparent Fee</h4>
+                                <p class="fs-5">
+                                    You pay us a fee based on your portfolio size.
+                                </p>
+                            </div>
+
+
                         </div>
-                         <br>
-                        <div class="col-md-12">
-                            <h4 class="text-captalize fw-bold ">Fair Fees that are Easy to understand</h4>
-                            <p class="fs-5">
-                                Our fee structure is simple: You pay a competitive flat fee that’s based on your portfolio’s size.
-                                This fee structure aligns our interests with yours. In other words, if you do better, we do better.
-                                Our investment approach isn’t swayed by incentives to sell you specific products because, unlike
-                                many brokers, we don’t earn commissions on trades. That means we have no incentive to “churn” your
-                                account with lots of buys and sells.
-                            </p>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4 class="text-captalize fw-bold ">Fair Fees that are Easy to understand</h4>
+                                <p class="fs-5">
+                                    Our fee structure is simple: You pay a competitive flat fee that’s based on your portfolio’s size.
+                                    This fee structure aligns our interests with yours. In other words, if you do better, we do better.
+                                    Our investment approach isn’t swayed by incentives to sell you specific products because, unlike
+                                    many brokers, we don’t earn commissions on trades. That means we have no incentive to “churn” your
+                                    account with lots of buys and sells.
+                                </p>
+                            </div>
                         </div>
-                       </div>
-                   </div>
+                    </div>
                 </div>
             </div>
         </div>
