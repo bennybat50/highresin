f0ee3c180d2b88d17bb6d85cf1b4328c3c20dc11
bennybat50 <benny.bat51@gmail.com>
2022-01-20 11:55:36 +0100

    Update Diversity

--- resources/views/diversity.blade.php
+++ resources/views/diversity.blade.php
@@ -1,10 +1,161 @@
 @extends('templates.public')
 @section('content')
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<h1>Diversity page</h1>
+<div class="diversity-welcome ">
+    <div class="welcome-sec text-center">
+        <div class="container">
+            <div class="p-5">
+                <h1 class="text-white"><b><i>Diversity & Inclusion</i></b> -</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="di-sections">
+    <div class="section1">
+        <div class="content-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="p-4">
+                            <br>
+                            <br>
+                            <h1>In 2017, HighResin formalized what had been for many years an informal <i><b>Diversity & Inclusion</b></i> strategy.</h1>
+                            <br>
+                            <br>
+                            <p>Intended to increase diversity and inclusion at the ﬁrm, focusing on female and underrepresented groups within our investment teams and officer-level positions, through enhanced and improved recruitment, talent development, education and awareness, we are employing a four-pillar strategy to achieve this goal:</p>
+
+                        </div>
+
+                    </div>
+                    <div class="col-md-6">
+                        <img src="{{ asset('assets/images/diversity/di-img1.jpg') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-section"></div>
+    </div>
+    <div class="section2">
+        <div class="container">
+            <div class="p-5 text-center">
+                <h3 class="text-white"><b><i>Retention & Development</i></b></h3>
+                <p class="text-white">Programs and employment benefits aimed at retaining and developing HighResin’s female and diverse populations</p>
+                <br>
+                <br>
+                <h1 class="text-white">Results from our four-pillar strategy are promising. We continue to make progress toward goals while supporting racial and social justice more broadly in our communities.
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="section3">
+        <div class="content-section"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2>Diversity</h2>
+                    <p>A wide range of experiences and voices which fully represent the communities in which we operate.</p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Equity</h2>
+                    <p>Fair access to advancement opportunities for everyone, and the ability to succeed as their best, authentic selves.</p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Inclusion </h2>
+                    <p>Policies and actions that provide a community where everyone belongs and has a voice.</p>
+                </div>
+            </div>
+        </div>
+        <div class="content-section"></div>
+    </div>
+    <div class="section2">
+        <div class="container">
+            <div class="p-5 text-center">
+                <img src="{{ asset('assets/images/diversity/quote-up.svg') }}" alt="" >
+                <h2 class="text-white">We all just want to be respected – to be treated with dignity; listened to; and heard. Once you understand that, you realize how simple it is to reach out and make connections. So as we face a daunting set of global challenges, we have to face them not as adversaries, but as fellow human beings.</h2>
+                <img src="{{ asset('assets/images/diversity/quote-down.svg') }}" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="section4">
+        <div class="content-section"></div>
+        <div class="container">
+            <div class="col-md-12 text-center">
+                <h1>Our DEI Strategy</h1>
+                <p>HighResin is committed to helping build a better, fairer society, starting with our own organization. We believe that a strong culture, in which inclusion and belonging are paramount, creates room for all of us to drive our purpose of helping more and more people experience financial well-being. Our DEI strategy is centered on three key pillars:</p>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Our talent & culture across the globe</h3>
+                    <p>Building, developing and retaining diverse talent and fostering a connected culture within a welcoming, inclusive and equitable environment.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Our role as fiduciary on behalf of clients</h3>
+                    <p>Leveraging ESG-focused financial products as competitive differentiators and strengthening client relationships by engaging them on DEI commitments.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Policy and social impact</h3>
+                    <p>Advancing policy and philanthropic initiatives that promote DEI and financial security within vulnerable communities to support upward mobility.</p>
+                </div>
+            </div>
+        </div>
+        <div class="content-section"></div>
+    </div>
+    <div class="section5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="{{ asset('assets/images/diversity/di-img2.jpg') }}" alt="">
+                </div>
+                <div class="col-md-6">
+                   <div class="p-5">
+                    <h1><b>As a fiduciary</b></h1>
+                    <p>We leverage DEI to better meet the needs of our clients. Learn more about our commitment to sustainability.</p>
+                    <br>
+                    <h2><i>Our commitments</i></h2>
+                    <br>
+                    <hr>
+                    <h3>To serve our clients as a DEI fiduciary</h3>
+                    <p>Increasing our partnerships with minority-owned business enterprises that support our client investment activities and third-party vendors across the firm.</p>
+                    <hr>
+                    <h3>To offer financial products as competitive differentiators</h3>
+                    <p>Developing and launching products focused on DEI and social impact across our active platform, private markets and iShares and index businesses.</p>
+                    <br>
+                   </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section6">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                   <div class="p-5">
+                       <h1><b>Our progress</b></h1>
+                       <br>
+                    <ul>
+                        <li>Our Diverse Broker Program helps to increase liquidity for clients by increasing trading with women-, minority-, and veteran-owned brokerage firms. It had a record year in 2020 with $295B notional trading volumes with diverse brokers.</li>
+                        <br>
+                        <li>We updated our Supplier Code of Conduct to enhance and clarify our expectations for Suppliers on important topics including DEI, human rights, labor standards, environmental sustainability and anti-corruption.</li>
+                        <br>
+                        <li>As part of our 2021 Investment stewardship priorities, we expect companies to disclose their approach to ensuring appropriate board diversity and, in those markets where we consider demographic diversity a priority, a demographic profile of the incumbent board.</li>
+                    </ul>
+                   </div>
+
+                </div>
+                <div class="col-md-6">
+                    <img src="{{ asset('assets/images/diversity/di-img3.jpg') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section2">
+        <div class="container">
+            <div class="p-5 text-center">
+                <img src="{{ asset('assets/images/diversity/quote-up.svg') }}" alt="" >
+                <h2 class="text-white">At HighResin, diversity, equity and inclusion is more than a strategy, it is a mindset embedded at the heart of everything we do – attracting and retaining talent, serving our clients through investment stewardship and financial products, and engaging communities outside of HighResin to create positive change.</h2>
+                <img src="{{ asset('assets/images/diversity/quote-down.svg') }}" alt="">
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
